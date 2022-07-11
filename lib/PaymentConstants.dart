9b65594303888ce83a9de18c2d7454beb4d8be56
AmrHaitham <amro88981@gmail.com>
2022-07-11 21:50:12 +0200

    change to new init id

--- lib/PaymentConstants.dart
+++ lib/PaymentConstants.dart
@@ -1,7 +1,7 @@
  String PaymobApiKey='ZXlKaGJHY2lPaUpJVXpVeE1pSXNJblI1Y0NJNklrcFhWQ0o5LmV5SnVZVzFsSWpvaWFXNXBkR2xoYkNJc0luQnliMlpwYkdWZmNHc2lPakV3TURJM05Dd2lZMnhoYzNNaU9pSk5aWEpqYUdGdWRDSjkuc2JCVGczWVRRR2lYZEhoRTYwSXJ4UXBZOVlpeElESjBUb0dkc1VsVlptei1NblRSTER6TFJHWURvNlBqZWJWZkZ0OUk2YlF6SGMyal9kOHNZcEpSY1E=';
  //real id
- // String IntegrationIDCard='2354395';
- // String IntegrationIDCardMobileWallet='2353241';
+ String IntegrationIDCard='2354395';
+ String IntegrationIDCardMobileWallet='2353241';
 
  String iframeID = '233854';
 //constats
@@ -21,5 +21,5 @@
 
 
  //test
- String IntegrationIDCard='253179';
-  String IntegrationIDCardMobileWallet='2344476';
+ // String IntegrationIDCard='253179';
+ //  String IntegrationIDCardMobileWallet='2344476';
